9d64d1265512111b04ddc6fb43508f7f6991a5f7
Baku Hashimoto <mail@baku89.com>
2023-12-04 16:03:02 +0900

    Store last sent values to localStorage in DMX.ts

--- src/stores/dmx.ts
+++ src/stores/dmx.ts
@@ -2,9 +2,12 @@ import {range} from 'lodash'
 import {defineStore} from 'pinia'
 
 import {useOscStore} from './osc'
+import {useAppConfigStore} from 'tweeq'
+import {watchEffect} from 'vue'
 
 export const useDmxStore = defineStore('dmx', () => {
 	const osc = useOscStore()
+	const appConfig = useAppConfigStore()
 
 	const senders = osc.senders(
 		Object.fromEntries(
@@ -16,6 +19,17 @@ export const useDmxStore = defineStore('dmx', () => {
 	)
 
 	const values = Object.values(senders)
+
+	const cachedValues = values.map((_, i) => {
+		return appConfig.ref(`dmx${i + 1}`, 1)
+	})
+
+	cachedValues.forEach((cache, i) => {
+		values[i].value = cache.value
+		watchEffect(() => {
+			cache.value = values[i].value
+		})
+	})
 
 	return {values}
 })
