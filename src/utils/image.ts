8dee14edcbbb521ed94f233d93a91a9790c0de2f
Baku Hashimoto <mail@baku89.com>
2023-12-05 16:22:24 +0900

    Remove exif orientation when making a lv thumbnail

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -3,6 +3,45 @@ import {vec2} from 'linearly'
 let canvas: HTMLCanvasElement | null = null
 let ctx: CanvasRenderingContext2D | null = null
 
+async function removeExifOrientation(blob: Blob): Promise<Blob> {
+	const dv = new DataView(await blob.arrayBuffer())
+	let offset = 0,
+		recess = 0
+
+	const pieces: {recess: number; offset: number}[] = []
+	let i = 0
+
+	if (dv.getUint16(offset) == 0xffd8) {
+		offset += 2
+		let app1 = dv.getUint16(offset)
+		offset += 2
+		while (offset < dv.byteLength) {
+			if (app1 == 0xffe1) {
+				pieces[i] = {recess: recess, offset: offset - 2}
+				recess = offset + dv.getUint16(offset)
+				i++
+			} else if (app1 == 0xffda) {
+				break
+			}
+			offset += dv.getUint16(offset)
+			app1 = dv.getUint16(offset)
+			offset += 2
+		}
+
+		if (pieces.length > 0) {
+			const newPieces: Blob[] = []
+			pieces.forEach(v => {
+				newPieces.push(blob.slice(v.recess, v.offset))
+			}, blob)
+			newPieces.push(blob.slice(recess))
+			return new Blob(newPieces, {type: 'image/jpeg'})
+		}
+	}
+
+	console.log('do nothing')
+	return blob
+}
+
 export async function resizeBlobImage(blob: Blob, size: vec2) {
 	if (!canvas) {
 		canvas = document.createElement('canvas')
@@ -15,7 +54,8 @@ export async function resizeBlobImage(blob: Blob, size: vec2) {
 
 	ctx = canvas.getContext('2d')!
 
-	const bitmap = await createImageBitmap(blob)
+	const blobDeorient = await removeExifOrientation(blob)
+	const bitmap = await createImageBitmap(blobDeorient)
 	ctx.drawImage(bitmap, 0, 0, width, height)
 	bitmap.close()
 
