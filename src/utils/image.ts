5a98dc95bc53fd2eeeeee73c003e27928b6e4a41
Baku Hashimoto <mail@baku89.com>
2023-12-05 16:08:53 +0900

    Reuse context in resizeBlobImage

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -1,6 +1,7 @@
 import {vec2} from 'linearly'
 
 let canvas: HTMLCanvasElement | null = null
+let ctx: CanvasRenderingContext2D | null = null
 
 export async function resizeBlobImage(blob: Blob, size: vec2) {
 	if (!canvas) {
@@ -12,8 +13,11 @@ export async function resizeBlobImage(blob: Blob, size: vec2) {
 	canvas.width = width
 	canvas.height = height
 
-	const ctx = canvas.getContext('2d')!
-	ctx.drawImage(await createImageBitmap(blob), 0, 0, width, height)
+	ctx = canvas.getContext('2d')!
+
+	const bitmap = await createImageBitmap(blob)
+	ctx.drawImage(bitmap, 0, 0, width, height)
+	bitmap.close()
 
 	return await new Promise<Blob>((resolve, reject) => {
 		canvas!.toBlob(b => (b ? resolve(b) : reject()), 'image/jpeg', 0.9)
